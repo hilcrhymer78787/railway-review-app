6af4f0321adcb201e082a94212d8be9d46782b0b
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2022-12-03 19:58:54 +0900

    test render login

--- src/page/login/index.test.tsx
+++ src/page/login/index.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from "../../App"
+import {BrowserRouter} from 'react-router-dom'
+import Login from "../login/index"
 
 it('render login', () => {
-  render(<App />);
+  render(<Login />, {wrapper: BrowserRouter});
   expect(screen.queryByTestId('email')).toBeTruthy()
   expect(screen.queryByTestId('password')).toBeTruthy()
   expect(screen.queryByTestId('submit')).toBeTruthy()
