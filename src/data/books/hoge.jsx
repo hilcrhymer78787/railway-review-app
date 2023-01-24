8d8754ad0b859fa7a1267c01e2f48654ab690e7a
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2023-01-24 17:13:27 +0900

    fix

--- src/data/books/hoge.jsx
+++ src/data/books/hoge.jsx
@@ -7,7 +7,6 @@ const Fetch = () => {
   React.useEffect(() => {
     const loadData = async () => {
       const result = await axios.get("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json");
-      console.log(result);
       setData(result.data);
     };
     loadData();
